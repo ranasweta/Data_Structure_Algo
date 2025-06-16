a908236faf9048985aad95ddef9c4a7865d2d881
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-06-16 13:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (38.11%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,15 +1,18 @@
 class Solution {
-public://brute force
+public://optimal just keep track of the minimum element and subtract it from its bigger one  
     int maximumDifference(vector<int>& nums) {
-        int mx=-1;
+        int r=-1;
+        int minele=nums[0];
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-            if(nums[j]<nums[i]){
-                mx=max(mx,nums[i]-nums[j]);
+            if(nums[i]<minele){
+                minele=nums[i];
+
             }
+            else if(nums[i]>minele){
+                r=max(r,nums[i]-minele);
             }
         }
-        return mx;
+    return r;
     }
 };
