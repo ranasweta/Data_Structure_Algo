df8ed9c63594f570715e8f65ba702f97ff556c2d
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-06-19 17:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (72.46%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,48 @@
 class Solution {
-public:
-int dp[101][101];
-int m,n;
-int solve(int i,int j,vector<vector<int>>& Grid){
-    if(i>m-1||i<0||j>n-1||j<0||Grid[i][j]==1) return 0;
-    if(i==m-1 && j==n-1) return 1;//1 path found
-    if(dp[i][j]!=-1) return dp[i][j];
-    int right=solve(i,j+1,Grid);
-    int down=solve(i+1,j,Grid);
-    return dp[i][j]=right+down;
-}
-    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
-        m=Grid.size();
-        n=Grid[0].size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,Grid);
+public://botttom up
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>t(m,vector<int>(n,0));
+        //1st row
+        for(int i=0;i<n;i++){
+            if(i>0 && grid[0][i-1]==1){
+                t[0][i]=0;
+                grid[0][i]=1;//marking this side as not reachabele
+            
+            }
+            else if(grid[0][i]==1){
+                t[0][i]=0;
+            }
+            else{
+                t[0][i]=1;
+            }
+        }
+        //1st col
+        for(int i=0;i<m;i++){
+            if(i>0 && grid[i-1][0]==1){
+                t[i][0]=0;
+                grid[i][0]=1;//marking this side as not reachabele
+            
+            }
+            else if(grid[i][0]==1){
+                t[i][0]=0;
+            }
+            else{
+                t[i][0]=1;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(grid[i][j]==1){
+                    t[i][j]=0;
+                }
+                else{
+
+                t[i][j]=t[i-1][j]+t[i][j-1];
+                }
+            }
+        }
+        return t[m-1][n-1];
     }
 };
