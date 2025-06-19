496eecac4fe439c67f501db1ef718bb93c307c6b
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-06-19 12:57:18 +0530

    Time: 27 ms (71.95%) | Memory: 31.3 MB (71.18%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,23 +1,19 @@
 class Solution {
-public://recur and memo
-
-int dp[501][501];
-int solve(int i,int j,string &s){
-    if(i>=j){
-        return 0;
-    }
-    
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(s[i]==s[j]){
-        return dp[i][j]=solve(i+1,j-1,s);
-    }
-    
-        return dp[i][j]=1+min(solve(i+1,j,s),solve(i,j-1,s));
-
-}
+public://blueprint
     int minInsertions(string s) {
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,n-1,s);
+        vector<vector<int>>t(n,vector<int>(n,0));
+        for(int l=2;l<=n;l++){
+            for(int i=0;i<n-l+1;i++){
+                int j=l+i-1;
+                if(s[i]==s[j]){
+                    t[i][j]=t[i+1][j-1];
+                }
+                else{
+                    t[i][j]=1+min(t[i][j-1],t[i+1][j]);
+                }
+            }
+        }
+        return t[0][n-1];//it will show that from 0->n-1 how many min inserrtion are needed
     }
 };
