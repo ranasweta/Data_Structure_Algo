d7e6e6ce3e036e447f48135957387146c27edc2e
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-06-22 13:36:17 +0530

    Time: 4 ms (1.65%) | Memory: 9.8 MB (44.22%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.cpp
@@ -4,8 +4,8 @@ public:
         int n=s.size();
         int cnt=0;
         vector<string> r;
-        for(int i=0;i<n;i++){
-            if(i%k==0){
+        for(int i=0;i<n;i+=k){
+            
                 if(i+k-1>n){
                     r.push_back(s.substr(i,n-i));
                 }
@@ -13,7 +13,7 @@ public:
 
                 r.push_back(s.substr(i,k));
                 }
-        }}
+        }
         if(r[r.size()-1].size()<k){
             while(r[r.size()-1].size()!=k){
             r[r.size()-1].push_back(fill);
