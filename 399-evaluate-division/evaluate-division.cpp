2f22e08ea067fb59a5d747c2d8628b86385d5052
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-06-24 15:55:51 +0530

    Time: 1 ms (30.38%) | Memory: 12 MB (39.84%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
-    void dfs(unordered_map<string, vector<pair<string,double>>>& adj,string src,string des,unordered_set<string>visited,double &ans,double &product){
-        if(visited.find(src)!=visited.end()){
-            return ;
-        }
-        visited.insert(src);
-        if(src==des){
-            ans=product;
-            return;
-        }
-        for(auto & p:adj[src]){
-            string v= p.first;
-            double w=p.second;
-            double newProduct = product * w;
-dfs(adj, v, des, visited, ans, newProduct);
-
-        }
-
-    }
+    void bfs(unordered_map<string, vector<pair<string, double>>>& adj,
+             string src, string des, unordered_set<string> visited, double& ans,
+             double& product) {
+                queue<pair<string,double>>que;//{string, product}
+                que.push({src,product});
+                visited.insert(src);
+                while(!que.empty()){
+                    string u=que.front().first;
+                    double prod=que.front().second;
+                    que.pop();
+                    for(auto & p:adj[u]){
+                        string v=p.first;
+                        double val=p.second;
+                        if(v==des){
+                            ans=prod*val;
+                        }
+                        if(visited.find(v)==visited.end()){
+                            visited.insert(v);
+                            double newprod=prod*val;
+                            que.push({v,newprod});
+                        }
+                    }
+                }
+             }
     vector<double> calcEquation(vector<vector<string>>& equations,
                                 vector<double>& values,
                                 vector<vector<string>>& queries) {
         int n = equations.size();
-        unordered_map<string, vector<pair<string,double>>> adj;
+        unordered_map<string, vector<pair<string, double>>> adj;
         for (int i = 0; i < n; i++) {
             string u = equations[i][0];
             string v = equations[i][1];
             double w = values[i];
-            adj[u].push_back({v, w}) ;
+            adj[u].push_back({v, w});
             adj[v].push_back({u, 1.0 / w});
         }
-        vector<double>result;
-        for(auto & query:queries){
-            string src=query[0];
-            string des=query[1];
-            double ans=-1.0;
-            double product=1.0;
+        vector<double> result;
+        for (auto& query : queries) {
+            string src = query[0];
+            string des = query[1];
+            double ans = -1.0;
+            double product = 1.0;
 
-            if(adj.find(src)!=adj.end()){
-                unordered_set<string>visited;
-                dfs(adj,src,des,visited,ans,product);
+            if (adj.find(src) != adj.end()) {
+                unordered_set<string> visited;
+                bfs(adj, src, des, visited, ans, product);
             }
             result.push_back(ans);
         }
