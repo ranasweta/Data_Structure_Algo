c0c43c3f320956a97cf0c63d10bca46d914d4bc9
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-07-11 19:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (92.33%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int gola_idx=-1;//if found or not 
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                gola_idx=i-1;
+                break;
+            }
+        }
+        if(gola_idx!=-1){int swap_idx=gola_idx;
+        for(int j=n-1;j>=gola_idx+1;j--){
+            if(nums[j]>nums[gola_idx]){
+                swap_idx=j;
+                break;
+            }
+        }
+        swap(nums[gola_idx],nums[swap_idx]);
+        reverse(nums.begin()+gola_idx+1,nums.end());
+        }
+        else{
+        reverse(nums.begin(),nums.end());//which means that whole string is 1st string
+            
+        }
+    }
+};
