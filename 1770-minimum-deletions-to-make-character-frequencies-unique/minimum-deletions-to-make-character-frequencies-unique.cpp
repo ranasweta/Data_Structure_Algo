6a694ef45dbb0aa52b70954a563f912f5d46d00c
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-07-19 23:31:50 +0530

    Time: 10 ms (49.50%) | Memory: 20.2 MB (89.79%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int minDeletions(string s) {
         int freq[26]={0};
-        for( char & ch:s){
+        for(char & ch:s){
             freq[ch-'a']++;
         }
         int result=0;
-        set<int>st;
-        for(int i=0;i<26;i++){
-            while(freq[i]>0 && st.find(freq[i])!=st.end()){
-                freq[i]--;
-                result++;
+        sort(begin(freq),end(freq));
+        for(int i=24;i>=0 && freq[i]>0;i--){
+            if(freq[i]>=freq[i+1]){
+            int prev=freq[i];
+            freq[i]=max(0,freq[i+1]-1);
+            result+=(prev-freq[i]);
+
             }
-            st.insert(freq[i]);
+
         }
         return result;
-
     }
 };
