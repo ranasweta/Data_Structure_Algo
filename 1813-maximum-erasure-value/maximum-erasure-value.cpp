0a05f281df39dd3245e7ebf4917d2799c7d6698c
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-07-22 17:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 100.6 MB (92.12%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n=nums.size();
-        unordered_set<int> st;
-        int sum=0;
-        int res=0;
-        int i=0,j=0;
-        while(j<n){
-            if(!st.count(nums[j])){
-                sum+=nums[j];
-                res=max(res,sum);
-                st.insert(nums[j]);
-                j++;
-            }
-            else{
-                //we have found the duplicate
-                while(i<n && st.count(nums[j])){
-                    sum-=nums[i];
-                    st.erase(nums[i]);
-                    i++;
-                }
-            }
+        vector<int> cumSum(n,0);
+        cumSum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            cumSum[i]=cumSum[i-1]+nums[i];
         }
-        return res;
+        vector<int> mp(10001,-1);
+        int result=0;
+        int i=0;
+        int j=0;
+        while(j<n)
+        {
+            i=max(i,mp[nums[j]]+1);
+            int jthSum=cumSum[j];
+            int ithSum=i-1<0?0:cumSum[i-1];
+            result=max(result,jthSum-ithSum);
+            mp[nums[j]]=j;
+            j++;
+        }
+        return result;
     }
 };
