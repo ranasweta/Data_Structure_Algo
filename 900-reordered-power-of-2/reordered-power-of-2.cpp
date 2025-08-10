2b5853ea23d9fcbb8da8d2b2604f2db284b45497
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-10 20:18:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (91.45%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<int> getVectorCountFormat(int n){
-        vector<int> v(10,0);
+    int numberFormat(int n){
+        int v=0;
         while(n){
-            v[n%10]++;
+            v+=pow(10,n%10);
             n/=10;
         }
         return v;
     }
     bool reorderedPowerOf2(int n) {
-        vector<int> inp=getVectorCountFormat(n);
+        int inp=numberFormat(n);
         for(int p=0;p<=29;p++){
-            if(inp==getVectorCountFormat(1<<p)){
+            if(inp==numberFormat(1<<p)){
                 return true;
             }
         }
