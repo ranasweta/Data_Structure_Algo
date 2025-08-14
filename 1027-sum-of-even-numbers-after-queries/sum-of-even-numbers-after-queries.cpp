8c55bb6839dce02e9d09f2ae637d5d38ff277d25
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-14 11:12:02 +0530

    Time: 19 ms (29.63%) | Memory: 54.2 MB (13.89%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    vector<int>ans;
-    void sum(vector<int>& nums,int idx,int val,int i){
-        nums[idx]+=val;
-        int temp=0;
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]%2==0){
-                temp+=nums[j];
+    vector<int> sumEvenAfterQueries(vector<int>& nums,
+                                    vector<vector<int>>& queries) {
+        int evenSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!(nums[i] % 2)) {
+                evenSum += nums[i];
             }
         }
-        ans[i]=temp;
-    }
-    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=queries.size();
-        ans.resize(queries.size(),0);
-        for(int i=0;i<n;i++){
-            int val=queries[i][0];
-            int idx=queries[i][1];
-            sum(nums,idx,val,i);
+        
+        vector<int> ans;
+        for (auto q : queries) {
+            int val = q[0];
+            int idx = q[1];
+            if (!(nums[idx] % 2)) {
+                evenSum -= nums[idx];
+            }
+            nums[idx] += val;
+            if (!(nums[idx] % 2)) {
+                evenSum += nums[idx];
+            }
+            ans.push_back(evenSum);
         }
         return ans;
     }
