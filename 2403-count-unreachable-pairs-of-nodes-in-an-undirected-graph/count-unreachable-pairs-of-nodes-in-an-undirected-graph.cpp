adce42815c5ec2ddddf73a6aedcb76890cc1c854
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-17 15:32:54 +0530

    Time: 257 ms (12.25%) | Memory: 224.1 MB (11.35%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,52 +1,36 @@
 class Solution {
-public:
-    vector<int> parent;
-    vector<int> rank;//baad me resize krn amat bhulna
-    int find(int x){
-        if(x==parent[x])
-           return x;
-        return find(parent[x]);
-    }
-    void Union(int x,int y){
-        int x_p=find(x);
-        int y_p=find(y);
-        if(x_p==y_p){
-            return ;
-        }
-        if(rank[x_p]>rank[y_p]){
-            parent[y_p]=x_p;
-        }
-        else if(rank[y_p]>rank[x_p]){
-            parent[x_p]=y_p;
-        }
-        else{
-            parent[x_p]=y_p;
-            rank[y_p]++;
+public://using dfs vese toh ye swal dsu se hi best solve hi skta hai
+    vector<bool> visited;
+        unordered_map<int,vector<int>> adj;
+        long long size;
+    void dfs(int u){
+        visited[u]=true;
+        size++;
+        for(auto & v: adj[u]){
+            if(!visited[v]){
+                dfs(v);
+            }
         }
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
-        parent.resize(n);
-        rank.resize(n,0);
+        for(auto edge: edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        visited=vector<bool>(n,false); 
+        int remaining=n;
+        long long res=0;
         for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-        for(auto &v:edges){
-            Union(v[0],v[1]);
+            if(!visited[i]){
+                size=0;
+                dfs(i);
+                res+=(size)*(remaining - size);
+                remaining-=size;
 
+            }
         }
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            int p=find(i);
-            mp[p]++;
-        }
-        long long r=0;
-        long long rem=n;
-        for(auto &it : mp){
-            int s=it.second;
-            r+=s*(rem-s);
-            rem-=s;
-
-        }
-        return r;
+        return res;
     }
 };
