abddac003c381c3fc2224a05d93222fb14120947
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-20 17:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (94.20%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,32 +1,64 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    DSU(int n) {
+        parent.resize(n+1); //1, 2, 3, 4, 5.., n
+        rank.resize(n+1);
+        for(int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int find(int x) {
+        if(x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]); //path compression
+    }
+
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) {
+            return;
+        }
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[y_parent] > rank[x_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[y_parent] = x_parent; //we made x as the parent
+            rank[x_parent]++;
+        }
+    }
+};
+
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>> &adj, int u,int v,vector<bool> &visited){
-        if(u==v){
-            return true;
-        }
-        visited[u]=true;
-        for(int x:adj[u]){
-            if(visited[x]) continue;
-                if(dfs(adj,x,v,visited)) return true;
-
-            
-        }
-        return false;
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n=edges.size();
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<n;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            vector<bool> visited(n,false);
-            if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end() && dfs(adj,u,v,visited)){
-                return edges[i];
-            }
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        int n = edges.size();
 
+        DSU dsu(n);
+        //T.C : DSU = alpha(n)
+
+        //T.C : O(n * alpha(n))
+        for(auto &edge : edges) { //O(n)
+            int u = edge[0];
+            int v = edge[1];
+
+            if(dsu.find(u) == dsu.find(v)) {
+                return edge;
+            }
+
+            dsu.Union(u, v);
         }
+
         return {};
     }
 };
