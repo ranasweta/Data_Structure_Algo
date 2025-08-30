11d59fd98e59989ed367d2f3a7f5c2b1d9b3e91b
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-30 18:52:41 +0530

    Time: 114 ms (2.85%) | Memory: 290.7 MB (5.34%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,14 +3,14 @@ public:
     int m,n;
     vector<vector<int>>dp;
     bool solve(string s,string t,int i,int j){
-        if(i==m) return true;
-        if(j==n) return false;
+        if(i==-1) return true;
+        if(j==-1) return false;
         if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==t[j]){
-            return dp[i][j]= solve(s,t,i+1,j+1);
+            return dp[i][j]= solve(s,t,i-1,j-1);
         }
         else{
-            return dp[i][j]= solve(s,t,i,j+1);
+            return dp[i][j]= solve(s,t,i,j-1);
 
         }
         
@@ -20,6 +20,6 @@ public:
         m=s.length();
         n=t.length();
         dp.resize(m+1,vector<int>(n+1,-1));
-        return solve(s,t,0,0);
+        return solve(s,t,m,n);
     }
 };
