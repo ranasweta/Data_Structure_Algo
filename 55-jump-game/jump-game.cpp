bfd4c4b31f26ffe8599e85322128d17d23dd769f
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-08-30 20:05:00 +0530

    Time: 444 ms (12.66%) | Memory: 55.7 MB (7.09%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int n;
-    vector<int> dp; // Use int to represent 0 = unvisited, 1 = true, -1 = false
+    vector<int> dp; // Use int to represent -1 = unvisited, 1 = true, 0 = false
 
     bool solve(vector<int>& nums, int idx) {
         if (idx >= n - 1) return true;
-        if (dp[idx] != 0) return dp[idx] == 1;
+        if (dp[idx] != -1) return dp[idx];
 
         int furthestJump = min(idx + nums[idx], n - 1);
         for (int i = idx + 1; i <= furthestJump; ++i) {
             if (solve(nums, i)) {
-                dp[idx] = 1;
-                return true;
+               
+                return dp[idx] = true;
             }
         }
 
-        dp[idx] = -1;
-        return false;
+   
+        return dp[idx] =false;
     }
 
     bool canJump(vector<int>& nums) {
         n = nums.size();
-        dp.resize(n, 0); // 0 = unvisited
+        dp.resize(n, -1); // -1 = unvisited
         return solve(nums, 0);
     }
 };
