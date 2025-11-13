bc6498d163df3e9b8ec6a3b451445ec29cc621f5
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-11-13 15:11:17 +0530

    Time: 10 ms (18.84%) | Memory: 18.8 MB (9.42%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public://brute greedy
+    int maxOperations(string s) {
+       vector<int> one;
+       int i=0;
+       while (i<s.size()){
+        int gr=0;
+        if(s[i]=='0') i++;
+        while(i<s.size() && s[i]=='1'){
+            gr++;
+            i++;
+        }
+        if(i<s.size() && gr>0){
+            one.push_back(gr);
+        }
+
+
+       }
+        int temp=0;
+        int ans=0;
+       for(int n: one){
+        temp+=n;
+        ans+=temp;
+       }
+       return ans;
+    }
+};
