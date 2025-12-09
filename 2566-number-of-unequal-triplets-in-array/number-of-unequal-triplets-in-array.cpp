0d29814daa0404037a75386ca0ad7c91345ec04e
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-12-09 13:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (28.41%) - LeetSync

--- 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
@@ -3,7 +3,7 @@ public:
     int unequalTriplets(vector<int>& nums) {
         unordered_map<int, int> m;
         for (int n : nums)
-            ++m[n];
+            m[n]++;
         int res = 0, left = 0, right = nums.size();
         for (auto [n, cnt] : m) {
             right -= cnt;
