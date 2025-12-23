79892e5e365c980e1d02539eb71ce27f435ba605
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2025-12-23 15:51:21 +0530

    Time: 116 ms (46.33%) | Memory: 166.9 MB (53.67%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public://this is the recursion code with the binary search algo
+    int n;
+    int t[100001][3];
+    int BS(vector<vector<int>>& events,int endTime){
+        int l=0;
+        int r=n-1;
+        int result=n;//if koi or nhi h
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(events[mid][0]>endTime){
+                result=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return result;
+    }
+    //count can be 0 1 2 
+    int solve(vector<vector<int>>& events,int i,int count){
+        if(count==2 || i>=n) return 0;
+        //if we take that quantity
+        if(t[i][count]!=-1) return t[i][count];
+        int nextValid=BS(events,events[i][1]);//sending end index
+        int take=events[i][2]+solve(events,nextValid,count+1);
+        int nottake=solve(events,i+1,count);
+        return t[i][count]=max(take,nottake);
+
+
+    }
+    int maxTwoEvents(vector<vector<int>>& events) {
+        n=events.size();
+        sort(begin(events),end(events));
+        int count=0;
+        memset(t,-1,sizeof(t));
+        return solve(events,0,count);
+    }
+};
