90c2527872a25483b8891fecd57929a1ed5f03f5
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2026-01-02 19:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (53.60%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public://pigeonhole technique as the num will occupy half of the array hence its very high chance that it will be in 3 consecutive (smallest meaningful subarray window size (3))
     int repeatedNTimes(vector<int>& A) {
         for (int i = 0; i < A.size() - 2; ++i)
             if (A[i] == A[i + 1] || A[i] == A[i + 2])
