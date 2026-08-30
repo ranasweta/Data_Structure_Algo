4cdea434ab15e709fc2e1007976f749066666583
Sweta Rana <155182389+ranasweta@users.noreply.github.com>
2026-08-30 13:42:08 +0530

    Time: 31 ms (20.54%) | Memory: 34.1 MB (11.48%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        numin=min(nums)
+        numax=max(nums)
+
+        indmin=0
+        indmax=0
+        for i in range(len(nums)):
+
+            if nums[i]==numin:
+                indmin=i
+            elif nums[i]==numax:
+                indmax=i
+            else:
+                continue
+        n=len(nums)
+        if indmin>indmax:
+            indmax,indmin=indmin,indmax
+        print(indmin," ",indmax)
+        ans=min([n-indmax+indmin+1,indmax+1,n-indmin])
+        return ans
